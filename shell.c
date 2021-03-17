1e03470205e549ed6ccca3de6477bf97f95d4a29
thaldrin <zprestoz@protonmail.com>
2021-03-16 20:27:33 -0400

    Added Parsing Function

--- shell.c
+++ shell.c
@@ -1,3 +1,4 @@
+// Preprocessor Directives
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,10 +7,18 @@
 #define CWD_ERROR "--> ERROR: Cannot get CWD\n"
 #define DELIM " \n"
 
+// Function Prototypes
 void printCurrentDir();
 void readLine(char *);
 void parse(char *, char *[]);
 
+// Main function - invocation of all other functions
+//		   input string is the input from the user
+//		   args is the argument vector that will be
+//		   filled up during the parsing function
+//		   user input will be split up and saved here
+//		   printCurrentDir will print the path to the
+// 		   current directory over and over until quit
 int main(int argc, char argv[]) {
 	char input[ARRAY_SIZE];
 	char *args[ARRAY_SIZE];
@@ -21,6 +30,8 @@ int main(int argc, char argv[]) {
 	return EXIT_SUCCESS;
 }
 
+// Function uses unistd.h to get the currend working directory
+// otherwise will return an error message, will probably never happen
 void printCurrentDir() {
 	char cwd[ARRAY_SIZE];
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
@@ -29,10 +40,17 @@ void printCurrentDir() {
 		printf( "%s\n", cwd );
 }
 
+// Function takes in the input string from main and fills in
+// the string with the user input including spaces using pass
+// by reference
 void readLine(char *input) {
 	scanf( "%[^\n]%*c", input );
 }
 
+// Uses the token function of string.h to parse through the
+// user input and tokenize each argument, saving them to the
+// argument vector args, both using pass by reference. This
+// function was inspired by a blog I read on geekstogeeks.com
 void parse(char *input, char *args[]) {
 	char *token;
 	const char delim[] = { DELIM };
@@ -44,3 +62,4 @@ void parse(char *input, char *args[]) {
 		args[argCount++] = token;
 	}
 }
+
