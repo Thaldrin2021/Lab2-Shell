dab33ac8a96aebf6fa79137320cf91c3e8d73fff
thaldrin <zprestoz@protonmail.com>
2021-03-16 20:21:03 -0400

    First Commit

--- shell.c
+++ shell.c
@@ -4,11 +4,20 @@
 #include <unistd.h>
 #define ARRAY_SIZE 1024
 #define CWD_ERROR "--> ERROR: Cannot get CWD\n"
+#define DELIM " \n"
 
 void printCurrentDir();
+void readLine(char *);
+void parse(char *, char *[]);
 
 int main(int argc, char argv[]) {
+	char input[ARRAY_SIZE];
+	char *args[ARRAY_SIZE];
 	printCurrentDir();
+	readLine(input);
+	parse(input, args);
+	for (int i = 0; args[i] != NULL; i++)
+		printf( "%s\n", args[i] );
 	return EXIT_SUCCESS;
 }
 
@@ -18,4 +27,20 @@ void printCurrentDir() {
 		printf( "%s", CWD_ERROR );
 	else
 		printf( "%s\n", cwd );
+}
+
+void readLine(char *input) {
+	scanf( "%[^\n]%*c", input );
+}
+
+void parse(char *input, char *args[]) {
+	char *token;
+	const char delim[] = { DELIM };
+	int argCount = 0;
+	token = strtok(input, delim);
+	args[argCount++] = token;
+	while (token != NULL) {
+		token = strtok(NULL, DELIM);
+		args[argCount++] = token;
+	}
 }
